f830508bfad44ea92b90c43118b092bcc0466fd0
yoondaeng <jyjo522i@gmail.com>
2023-01-02 21:43:29 +0900

    백엔드에서 로그인기능 구현

--- app.js
+++ app.js
@@ -12,9 +12,13 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
-app.use(express.static(`${__dirname}/src/public`));
 // 정적 경로 추가 디렉토리 네임은 app.js 파일이 있는 위치 안에 있는 퍼블릭을 정적 경로로 추가
+app.use(express.static(`${__dirname}/src/public`));
+
+// bodyParser를 사용할 때 미들 웨어를 등록해줘야함
 app.use(bodyParser.json());
+// URL를 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결을 위한 코드
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use("/", home); // us: 미들 웨어를 등록해주는 메서드
 
--- src/routes/home/home.ctrl.js
+++ src/routes/home/home.ctrl.js
@@ -1,7 +1,9 @@
 "use strict";
 
+
 const output = {
-    home: (req, res) => {
+    home: (req, res) => { 
+        // home, login: 해당 페이지를 렌더링해주는 함수
         res.render("home/index");
     },
     
@@ -10,9 +12,30 @@ const output = {
     },
 };
 
+const users = {
+    id: ["admin", "12201953", "user"],
+    password: ["0000", "0000", "0000" ],
+};
+
 const process = {
      login: (req, res) => {
-        console.log(req.body);
+        const id = req.body.id,
+          password = req.body.password;
+
+        // 아이디와 패스워드 검증
+        if (users.id.includes(id)) { // 프론트에서 전달한 id가 users id에 있으면
+            const idx = users.id.indexOf(id); // id index를 가져옴
+            if (users.password[idx] === password){
+                return res.json({ // 프론트에게 로그인 성공 여부를 전달
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인에 실패했습니다. ",
+        });
      },
 };
 
