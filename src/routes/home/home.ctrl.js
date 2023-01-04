c091ad56c3eb0745ccaa895345494dfda4500ec6
yoondaeng <jyjo522i@gmail.com>
2023-01-04 20:18:55 +0900

    회원가입 페이지 구현

--- src/routes/home/home.ctrl.js
+++ src/routes/home/home.ctrl.js
@@ -11,7 +11,11 @@ const output = {
     login: (req, res) => {
         res.render("home/login");
     },
-};
+    
+    register: (req, res) => {
+        res.render("home/register");
+    },
+}
 
 const process = {
      login: (req, res) => {
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -7,7 +7,10 @@ const ctrl = require("./home.ctrl");
 
 router.get("/", ctrl.output.home);
 router.get("/login", ctrl.output.login);
+router.get("/register", ctrl.output.register);
+
 router.post("/login", ctrl.process.login);
+
 
 // 라우터를 사용할 수 있도록 외부로 보내줌
 module.exports = router;
