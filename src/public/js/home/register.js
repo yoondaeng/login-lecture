f0d4f55e8e0cb94566079abcb9235f357dede737
yoondaeng <jyjo522i@gmail.com>
2023-01-05 20:33:40 +0900

    회원가입 요구현

--- src/public/js/home/register.js
+++ src/public/js/home/register.js
@@ -1,18 +1,24 @@
 "use strict";
 
 const id = document.querySelector("#id"),
+  name = document.querySelector("#name"),
   password = document.querySelector("#password"),
-  loginBtn= document.querySelector("#button");
+  confirmpassword = document.querySelector("#confirm-password"),
+  registerBtn= document.querySelector("#button");
 
-loginBtn.addEventListener("click", login);
+registerBtn.addEventListener("click", register);
 
-function login() {
+function register() {
     const req = {
         id: id.value,
+        name: name.value,
         password: password.value,
+        confirmpassword: confirmpassword.value,
+
     };
+
     
-    fetch("/login", {
+    fetch("/register", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -21,13 +27,13 @@ function login() {
     })
       .then((res) => res.json())
       .then((res)=> {
-        if (res.success){ // 로그인 성공하면
-            location.href = "/"; // 루트로 이동
+        if (res.success){ // 회원가입 성공하면
+            location.href = "/login"; // 로그인 페이지로 이동
         } else {
             alert(res.msg); // 실패하면 메시지 출력
         }
       })
       .catch((err) => {
-        console.error("로그인 중 에러 발생");
+        console.error("회원가입 중 에러 발생");
       });
 }
